c0855236924669c34efc3bdb3ea25d858df4d94d
Omar Naja <s1097765@studenti.univpm.it>
2023-04-08 23:57:26 +0200

    Button salta

--- app/src/main/java/it/omarkiarafederico/skitracker/WelcomeActivity.kt
+++ app/src/main/java/it/omarkiarafederico/skitracker/WelcomeActivity.kt
@@ -1,12 +1,22 @@
 package it.omarkiarafederico.skitracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val saltaButton = findViewById<Button>(R.id.button2)
+        saltaButton.setOnClickListener{
+            val Intent = Intent(this, MapActivity::class.java)
+            startActivity(Intent)
+        }
     }
+
+
 }
