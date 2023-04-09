46b84994e8d90a89bf9638e3d7b0736f1e9df33c
Omar Naja <s1097765@studenti.univpm.it>
2023-04-09 18:22:35 +0200

    Inizio tutorial

--- app/src/main/java/it/omarkiarafederico/skitracker/view/activity/WelcomeActivity.kt
+++ app/src/main/java/it/omarkiarafederico/skitracker/view/activity/WelcomeActivity.kt
@@ -3,15 +3,26 @@ package it.omarkiarafederico.skitracker.view.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import androidx.fragment.app.FragmentTransaction
 import it.omarkiarafederico.skitracker.R
+import it.omarkiarafederico.skitracker.view.fragment.Guida1Fragment
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
+        val tutorialButton = findViewById<Button>(R.id.button)
         val saltaButton = findViewById<Button>(R.id.button2)
+
+        tutorialButton.setOnClickListener{
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.welcome, Guida1Fragment()).commit()
+        }
+
+
         saltaButton.setOnClickListener{
             val Intent = Intent(this, MapActivity::class.java)
             startActivity(Intent)
--- app/src/main/java/it/omarkiarafederico/skitracker/view/fragment/Guida1Fragment.kt
+++ app/src/main/java/it/omarkiarafederico/skitracker/view/fragment/Guida1Fragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import it.omarkiarafederico.skitracker.R
 
+/*
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -17,7 +19,12 @@ private const val ARG_PARAM2 = "param2"
  * Use the [Guida1Fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+ */
 class Guida1Fragment : Fragment() {
+
+    /*
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -30,13 +37,23 @@ class Guida1Fragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    */
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_guida1, container, false)
+      //  return inflater.inflate(R.layout.fragment_guida1, container, false)
+      val view = inflater.inflate(R.layout.fragment_guida1, container, false)
+      val nextBtn : Button = view.findViewById<Button>(R.id.button5)
+        nextBtn.setOnClickListener{
+            val fragment = Guida2Fragment()
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.guida1, fragment)?.commit()
+        }
+      return view
     }
+
+    /*
 
     companion object {
         /**
@@ -48,13 +65,14 @@ class Guida1Fragment : Fragment() {
          * @return A new instance of fragment Guida1Fragment.
          */
         // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Guida1Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        @JvmStatic fun newInstance(param1: String, param2: String) =
+                Guida1Fragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
                 }
-            }
     }
+
+     */
 }
