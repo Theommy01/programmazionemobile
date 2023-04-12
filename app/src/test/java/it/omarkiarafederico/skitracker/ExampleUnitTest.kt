60e54d77222b58a6963ef98dbee75000b22fdce7
Omar Naja <s1097765@studenti.univpm.it>
2023-04-12 13:26:26 +0200

    Utilizzo JUnit per testare lo switch map-aboutus activities

--- app/src/test/java/it/omarkiarafederico/skitracker/ExampleUnitTest.kt
+++ app/src/test/java/it/omarkiarafederico/skitracker/ExampleUnitTest.kt
@@ -1,8 +1,19 @@
 package it.omarkiarafederico.skitracker
 
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import it.omarkiarafederico.skitracker.view.activity.AboutUsActivity
+import it.omarkiarafederico.skitracker.view.activity.MapActivity
+import org.junit.After
 import org.junit.Test
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.runner.RunWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -15,3 +26,36 @@ class ExampleUnitTest {
         assertEquals(4, 2 + 2)
     }
 }
+
+@RunWith(AndroidJUnit4::class)
+class MyActivityTest {
+
+    private lateinit var scenario: ActivityScenario<MapActivity>
+
+    @Before
+    fun setUp() {
+        // Avvia lo scenario dell'attività da testare
+        scenario = ActivityScenario.launch(MapActivity::class.java)
+        // Inizializza Intents per la verifica delle intent
+        Intents.init()
+    }
+
+    @After
+    fun tearDown() {
+        // Chiude lo scenario dell'attività da testare
+        scenario.close()
+        // Rilascia Intents dopo il test
+        Intents.release()
+    }
+
+    @Test
+    fun testStartActivity() {  //Per verificare il corretto funzionamento del passaggio da MapActivity
+        //ad AboutUsActivity, su cui ci sono state delle problematiche
+
+        // Esegui la funzione startActivity
+        scenario.onActivity { it.startActivity(Intent(it, AboutUsActivity::class.java)) }
+        // Verifica che sia stata lanciata un'Intent per l'attività MyOtherActivity
+        intended(hasComponent(AboutUsActivity::class.java.name))
+    }
+}
+
