52595e9fe153fc692f60e736fc94a694806d4844
Omar Naja <s1097765@studenti.univpm.it>
2023-04-30 15:24:02 +0200

    Quasi completata parte iniziale della grafica del fragment info piste

--- app/src/main/java/it/omarkiarafederico/skitracker/view/skimap/InfoPisteFragment.kt
+++ app/src/main/java/it/omarkiarafederico/skitracker/view/skimap/InfoPisteFragment.kt
@@ -7,8 +7,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import it.omarkiarafederico.skitracker.R
 
@@ -28,21 +30,42 @@ class InfoPisteFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val titolo: TextView = view.findViewById(R.id.titolo)
-        titolo.text = "MODIFICA" //modifica di prova
-        // val numPiste
-        // val impianti risalita
-        // val altitudinemin
-        // val altitudinemax
-        // val sito
+        val numPiste: TextView = view.findViewById(R.id.numPiste)
+        val impiantiRisalita: TextView = view.findViewById(R.id.impiantiRisalita)
+        val max: TextView = view.findViewById(R.id.altMax)
+        val min: TextView = view.findViewById(R.id.altMin)
+        val sito: TextView = view.findViewById(R.id.website)
+
 
         /*
         var comprensorio = INSERIRE QUI IL NOME DEL COMPRENSORIO
         titolo.text = "$comprensorio"
+        numPiste.text = "$numPiste"
+        etc.
 
          */
 
         //QUI CI ANDRANNO INSERITI TUTTI I DETTAGLI. PRENDEREMO I CODICI DI OGNI SINGOLA
         //TEXTVIEW E NE MODIFICHEREMO I CONTENUTI
+
+        titolo.text = "NOME" //modifica di prova
+        numPiste.text = "7"
+        impiantiRisalita.text = "12"
+        max.text = "100"
+        min.text = "0"
+        sito.text = "SITO UFFICIALE"
+
+        //Personalizzo il pulsante che indica se quel comprensorio è aperto o chiuso
+        val stato = view.findViewById<Button>(R.id.stato)
+        val aperto = true //o false ovviamente, ora true per prova
+
+        if (aperto) {
+            stato.text="Aperto"
+            stato.backgroundTintList = context?.let { ContextCompat.getColorStateList(it, R.color.green) }
+        } else {
+            stato.text="Chiuso"
+            stato.backgroundTintList = context?.let { ContextCompat.getColorStateList(it, R.color.red) }
+        }
     }
 
 }
