b4cac4b4d98d510afeea934e5081dc5db50dd321
Omar Naja <s1097765@studenti.univpm.it>
2023-05-06 15:36:46 +0200

    Database locale funzionante anche per Omar

--- app/src/main/java/roomdb/RoomDB.kt
+++ app/src/main/java/roomdb/RoomDB.kt
@@ -16,7 +16,7 @@ import androidx.room.*
     Comprensorio::class,
     PuntoMappa::class,
     PuntiMappaTracciamenti::class
-], version = 3)
+], version = 4)
 abstract class LocalDB: RoomDatabase() {
     abstract fun localDatabaseDao(): LocalDatabaseDao
 }
